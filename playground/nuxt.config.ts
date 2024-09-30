860d0d6d2fc99d660885a5fcffac6bee827ea9e3
ManuelPaglia <77502117+ManuelPaglia@users.noreply.github.com>
2024-09-30 17:59:21 +0200

    add dbName
    Aggiunto dbname alla conf

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  myModule: {},
   devtools: { enabled: true },
+
+  odooRpc: {
+    baseUrl: process.env.BASE_URL,
+    dbName: process.env.DB_NAME,
+  },
+
+  compatibilityDate: '2024-09-30',
 })
--- src/module.ts
+++ src/module.ts
@@ -2,18 +2,30 @@ import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
 export interface ModuleOptions {
   baseUrl?: string
+  dbName?: string
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'nuxt-odoo-rpc',
+    name: 'nuxt-odoo',
     configKey: 'odooRpc',
   },
   defaults: {
     baseUrl: '',
+    dbName: '',
   },
-  setup(options, nuxt) {
+  async setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
+
+    if (!options.baseUrl) {
+      await nuxt.close()
+      throw new Error('baseUrl required')
+    }
+
+    if (!options.dbName) {
+      await nuxt.close()
+      throw new Error('dbName required')
+    }
 
     nuxt.options.runtimeConfig.public.odooBaseUrl = options.baseUrl
 
